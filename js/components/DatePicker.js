e3d145b550777145488d26d76c622f20344e4349
dj51234 <dj51234@yahoo.com>
2026-02-04 02:37:00 -0500

    code and comment cleanup

--- js/components/DatePicker.js
+++ js/components/DatePicker.js
@@ -49,11 +49,13 @@ export default class DatePicker {
     this.renderCalendar()
   }
   changeMonth(dir) {
+    // add or subtract a month
     this.viewMonth += dir
-
+    // if viewmonth becomes less than 0, go to last year and december
     if (this.viewMonth < 0) {
       this.viewMonth = 11
       this.viewYear -= 1
+      // if viewMonth goes to 12, set month to January and increase to enxt year
     } else if (this.viewMonth > 11) {
       this.viewMonth = 0
       this.viewYear += 1
@@ -66,7 +68,10 @@ export default class DatePicker {
       month: 'short',
       day: 'numeric',
     })
+    // render formatted date
     this.displaySpan.textContent = formattedDate
+
+    // format for value to interpret the date
     const yyyy = this.selectedDate.getFullYear()
     const mm = String(this.selectedDate.getMonth() + 1).padStart(2, '0')
     const dd = String(this.selectedDate.getDate()).padStart(2, '0')
@@ -91,41 +96,42 @@ export default class DatePicker {
       'Dec',
     ]
 
+    // update the display at top of calendar
     this.currentMonthDisplay.textContent = `${months[this.viewMonth]} ${this.viewYear}`
 
-    const firstDayOfMonthIndex = new Date(
-      this.viewYear,
-      this.viewMonth,
-      1,
-    ).getDay()
-    const totalDaysInMonth = new Date(
-      this.viewYear,
-      this.viewMonth + 1,
-      0,
-    ).getDate()
+    // get the first day of the month (1)
+    const firstDayOfMonthIndex = new Date(this.viewYear, this.viewMonth, 1).getDay()
+    // to get total days in month get next month's 0th day
+    const totalDaysInMonth = new Date(this.viewYear, this.viewMonth + 1, 0).getDate()
 
+    // create empty spots before 1st day of month if not Sunday
     for (let i = 0; i < firstDayOfMonthIndex; i++) {
       const emptyCell = document.createElement('div')
       emptyCell.classList.add('calendar-day', 'empty')
       this.grid.appendChild(emptyCell)
     }
+    // fill in days to last day of month
     for (let i = 1; i <= totalDaysInMonth; i++) {
       const cell = document.createElement('div')
       cell.classList.add('calendar-day')
       cell.textContent = i
-
+      // if i is the day (number day) of the selected date && the month and year are equal
       if (
         i === this.selectedDate.getDate() &&
         this.viewMonth === this.selectedDate.getMonth() &&
         this.viewYear === this.selectedDate.getFullYear()
       ) {
+        // add selected class
         cell.classList.add('selected')
       }
 
       cell.addEventListener('click', (e) => {
         e.stopPropagation()
+        // on click update the calendar display
         this.selectedDate = new Date(this.viewYear, this.viewMonth, i)
+        // update the hidden input value
         this.updateDisplay()
+        // close and rerender calendar
         this.container.classList.remove('active')
         this.renderCalendar()
       })
--- js/components/DeleteModal.js
+++ js/components/DeleteModal.js
@@ -1,5 +1,6 @@
 export default class DeleteModal {
   constructor() {
+    // elements
     this.dialogue = document.getElementById('delete-modal')
     this.cancelBtn = document.getElementById('cancel-delete-btn')
     this.confirmBtn = document.getElementById('confirm-delete-btn')
@@ -7,6 +8,7 @@ export default class DeleteModal {
 
     // confirm delete callback
     this.onConfirm = null
+
     this.init()
   }
   init() {
@@ -14,7 +16,7 @@ export default class DeleteModal {
     this.dialogue.addEventListener('click', this.closeOnOverlayClick.bind(this))
     this.confirmBtn.addEventListener('click', (e) => {
       e.preventDefault()
-
+      // if confirm callback exists, run it
       if (this.onConfirm) {
         this.onConfirm()
       }
@@ -23,24 +25,31 @@ export default class DeleteModal {
     })
   }
   open(invoiceId, confirmCallback) {
+    // render invoice id in modal to confirm deletion
     if (this.invoiceId) {
       this.invoiceId.textContent = invoiceId
     }
+    // run callback
     this.onConfirm = confirmCallback
+    // open modal
     this.dialogue.showModal()
   }
   close(e) {
     e.preventDefault()
+    // close modal and reset callback to null
     this.dialogue.close()
     this.onConfirm = null
   }
   closeOnOverlayClick(e) {
+    // get bounds of inner modal
     const bounds = e.target.getBoundingClientRect()
+    // if click is inside modal
     const clickedInDialogue =
       e.clientY >= bounds.top &&
       e.clientY <= bounds.bottom &&
       e.clientX >= bounds.left &&
       e.clientX <= bounds.right
+    // close if outside bounds
     if (!clickedInDialogue) {
       this.close(e)
     }
